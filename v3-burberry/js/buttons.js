f4ff695c6bc3eefbffb74d5b8122560e2ed64269
Can Akburu <yesmancan_0611@hotmail.com>
2021-08-19 16:07:17 +0300

    update

---
+++ v3-burberry/js/buttons.js
@@ -0,0 +1,74 @@
+{
+    "types":
+    [
+        {
+            "type": "home",
+            "buttons": [
+                {
+                    "title": {
+                        "tr": "Blok Yönetimi",
+                        "en": "Block Management"
+                    },
+                    "icon": "block",
+                    "link": "/srv/admin/block/block/index",
+                    "click": "route"
+                },
+                {
+                    "title": {
+                        "tr": "Link Yönetimi",
+                        "en": "Link Management"
+                    },
+                    "icon": "link",
+                    "link": "/srv/admin/link/link/seo-edit/page/1",
+                    "click": "route"
+                },
+                {
+                    "title": {
+                        "tr": "Genel Ayarlar",
+                        "en": "General Settings"
+                    },
+                    "icon": "settings",
+                    "link": "/srv/admin/settings/general-settings/settings?settingCategory=1",
+                    "click": "route"
+                }
+            ]
+        },
+        {
+            "type": "category",
+            "buttons": [
+                {
+                    "title": {
+                        "tr": "Blok Yönetimi",
+                        "en": "Block Management"
+                    },
+                    "icon": "block",
+                    "link": "/srv/admin/block/block/index?SayfaId=2&AltSayfaId=",
+                    "click": "route"
+                },
+                {
+                    "title": {
+                        "tr": "Kategori Yönetimi",
+                        "en": "Category Management"
+                    },
+                    "icon": "pencil",
+                    "link": "/srv/admin/category/category/index",
+                    "click": "route"
+                }
+            ]
+        },
+        {
+            "type": "product",
+            "buttons": [
+                {
+                    "title": {
+                        "tr": "Ürün Yönetimi",
+                        "en": "Product Management"
+                    },
+                    "icon": "pencil",
+                    "link": "/srv/admin/products/save-edit/index/?id=",
+                    "click": "route"
+                }
+            ]
+        }
+    ]
+}
